d063b7372e27eebe1fa625d56763f02a8494641f
Crystal Santos <crystal.santos@liferay.com>
2021-01-08 12:20:57 -0300

    Add elastic configuration

--- elasticsearch/elasticsearch_startup.sh
+++ elasticsearch/elasticsearch_startup.sh
@@ -15,7 +15,7 @@ register_license() {
 
   sleep 30
   echo "Configuring Elasticsearch password"
-  echo -e "y\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o" | sh /usr/share/elasticsearch/bin/elasticsearch-setup-passwords interactive
+  #echo -e "y\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o\nClaro$2o2o" | sh /usr/share/elasticsearch/bin/elasticsearch-setup-passwords interactive
 
   echo "Registering Elasticsearch license"
   curl \
